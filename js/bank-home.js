94ac607c68335c9e8810dac1b0acca8740ff93a7
Joy Debnath <96875662+developerjoydebnath@users.noreply.github.com>
2022-02-11 14:46:54 +0600

    js updated

--- js/bank-home.js
+++ js/bank-home.js
@@ -1,25 +1,39 @@
+
+function inputValue(inputId){
+    const valueOfAmmount = document.getElementById(inputId).value;
+    const parseAmmount = parseFloat(valueOfAmmount);
+    document.getElementById(inputId).value = '';
+    return parseAmmount;
+}
+
+function totalValue(totalId){
+    const total = document.getElementById(totalId);
+    const parseTotal = parseFloat(total.innerText);
+    return parseTotal;
+}
+
+
+
 document.getElementById('deposite-btn').addEventListener('click', function () {
-    let depositeAmmount = document.getElementById('deposite-ammount').value;
-    let depositeTotal = document.getElementById('deposit-total');
-    let balanceTotal = document.getElementById('balance-total');
-    let parseDpTotal = parseFloat(depositeTotal.innerText);
-    let parseBlTotal = parseFloat(balanceTotal.innerText);
-    let parseDpAmmount = parseFloat(depositeAmmount);
-    depositeTotal.innerText = parseDpTotal + parseDpAmmount;
-    balanceTotal.innerText = parseBlTotal + parseDpAmmount;
-    document.getElementById('deposite-ammount').value = '';
+    const depositeAmmount = inputValue('deposite-ammount');
+    const depositeTotal = document.getElementById('deposit-total');
+    const parseDpTotal = totalValue('deposit-total');
+    const balanceTotal = document.getElementById('balance-total');
+    const parseBlTotal = totalValue('balance-total');
+    
+    depositeTotal.innerText = parseDpTotal + depositeAmmount;
+    balanceTotal.innerText = parseBlTotal + depositeAmmount;
 
 })
 
 document.getElementById('withdraw-btn').addEventListener('click', function () {
-    let withdrawAmmount = document.getElementById('withdraw-ammount').value;
-    let withdrawTotal = document.getElementById('withdraw-total');
-    let balanceTotal = document.getElementById('balance-total');
-    let parseBlTotal = parseFloat(balanceTotal.innerText);
-    let parseWdTotal = parseFloat(withdrawTotal.innerText);
-    let parseWdAmmount = parseFloat(withdrawAmmount);
-    withdrawTotal.innerText = parseWdTotal + parseWdAmmount;
-    balanceTotal.innerText = parseBlTotal - parseWdAmmount;
-    document.getElementById('withdraw-ammount').value = '';
+    const withdrawAmmount = inputValue('withdraw-ammount');
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const parseWdTotal = totalValue('withdraw-total');
+    const balanceTotal = document.getElementById('balance-total');
+    const parseBlTotal = totalValue('balance-total');
+
+    withdrawTotal.innerText = parseWdTotal + withdrawAmmount;
+    balanceTotal.innerText = parseBlTotal - withdrawAmmount;
 
 })
